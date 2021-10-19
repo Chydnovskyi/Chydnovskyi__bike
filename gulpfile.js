fe6dad15e2e1492ace726c367bf84c90a89f60d9
Chydnvoskyi <ChydnovskyiMV@ya.ru>
2021-10-19 10:53:28 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,15 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
-const webp = require("gulp-webp");
-const squoosh = require("gulp-libsquoosh");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
-const del = require("del");
+const htmlmin = require("gulp-htmlmin");
 const terser = require("gulp-terser");
+const squoosh = require("gulp-libsquoosh");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const sync = require("browser-sync").create();
 
 // Styles
 
@@ -31,25 +33,72 @@ const styles = () => {
 
 exports.styles = styles;
 
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+}
+
 // Scripts
 
 const scripts = () => {
-  return gulp.src("source/js/script.js")
-    .pipe(terser())
-    .pipe(rename("script.min.js"))
+  return gulp.src("source/js/*.js")
     .pipe(gulp.dest("build/js"))
     .pipe(sync.stream());
 }
 
 exports.scripts = scripts;
 
+// Images
+
+const optimizeImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(squoosh())
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = optimizeImages;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = copyImages;
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/icons/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.sprite = sprite;
+
 // Copy
 
 const copy = (done) => {
   gulp.src([
     "source/fonts/*.{woff2,woff}",
-    "source/*.ico",
-    "source/*.html",
+    'source/favicon/**',
+    "source/img/**/*.svg",
+    "!source/img/icons/*.svg",
   ], {
     base: "source"
   })
@@ -59,12 +108,18 @@ const copy = (done) => {
 
 exports.copy = copy;
 
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -75,51 +130,33 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
-
-// WebP
-
-const createWebp = () => {
-  return gulp.src("source/img/**/*.{jpg,png}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"))
-}
-
-exports.createWebp = createWebp;
-
-// Images
-
-const optimizeImages = () => {
-  return gulp.src("source/img/**/*.{png,jpg,svg,webp}")
-    // .pipe(squoosh())
-    .pipe(gulp.dest("build/img"))
-}
-
-exports.images = optimizeImages;
-
-// Clean
-
-const clean = () => {
-  return del("build");
-};
-
-exports.clean = clean;
 
 // Build
 
 const build = gulp.series(
   clean,
   copy,
-  createWebp,
   optimizeImages,
   gulp.parallel(
     styles,
-    scripts
+    html,
+    scripts,
+    sprite,
+    createWebp
   ),
 );
 
@@ -129,9 +166,15 @@ exports.build = build;
 
 
 exports.default = gulp.series(
+  clean,
+  copy,
+  copyImages,
   gulp.parallel(
     styles,
-    scripts
+    html,
+    scripts,
+    sprite,
+    createWebp
   ),
   gulp.series(
     server,
--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,9 @@ const headerBtn = document.querySelector('.header__btn');
 const headerNav = document.querySelector('.header__nav');
 const headerLogo = document.querySelector('.header__img');
 const header = document.querySelector('.header');
+const input = document.querySelector('#tel');
+
+// Меню
 
 headerNav.classList.remove('header__nav--show');
 header.classList.remove('header--no-js');
@@ -9,6 +12,40 @@ headerBtn.classList.remove('header__btn--close');
 
 headerBtn.addEventListener('click', function(evt) {
   evt.preventDefault();
+  header.classList.toggle('header--fullscreen');
   headerNav.classList.toggle('header__nav--show');
   headerBtn.classList.toggle('header__btn--close');
+});
+
+// Валидность формы
+
+function validatePhone(input){
+  let regex = /^\d[\d\(\)\ -]{4,14}\d$/;
+  return regex.test(input.value);
+  }
+
+let form = document.querySelector('.promo__submit');
+
+form.addEventListener('click', function(evt) {
+  if (!validatePhone(input)){
+    evt.preventDefault();
+    input.style.border = '3px solid #ee0909';
+  }else{
+    input.style.border = '1px solid #0ad9c6';
+  }
+});
+
+// Первый опыт с jQuery, не нашел лучшего варианта прокрутки к якорю
+
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (t) {
+  if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+      var e = $(this.hash);
+      e = e.length ? e : $("[name=" + this.hash.slice(1) + "]"), e.length && (t.preventDefault(), $("html, body").animate({
+          scrollTop: e.offset().top
+      }, 600, function () {
+          var t = $(e);
+          if (t.focus(), t.is(":focus")) return !1;
+          t.attr("tabindex", "-1"), t.focus()
+      }))
+  }
 });
